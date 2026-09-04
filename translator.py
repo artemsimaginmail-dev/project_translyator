d8c1680cf359c0880d3fe0f52cf445f7ca66f041
artemsimaginmail-dev <artemsimaginmail@gmail.com>
2026-09-04 07:19:29 +0300

    support bilingual type declaration

---
+++ translator.py
@@ -0,0 +1,9 @@
+KIND_WORDS = {
+    "integer": "integer",
+    "int": "integer",
+    "целые": "integer",
+    "real": "real",
+    "double": "real",
+    "float": "real",
+    "вещественные": "real",
+}
