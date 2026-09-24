2baa14fb6807730146237ba40f07a723c0955564
artemsimaginmail-dev <artemsimaginmail@gmail.com>
2026-09-24 20:50:22 +0300

    feat(web): настроили запуск Flask-сервера

--- web_app.py
+++ web_app.py
@@ -19,4 +19,6 @@ def main() -> int:
     parser.add_argument("--host", default="127.0.0.1")
     parser.add_argument("--port", type=int, default=5000)
     args = parser.parse_args()
-    
+    app.run(host=args.host, port=args.port, debug=False, use_reloader=False)
+    return 0
+
